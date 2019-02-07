fe4fc6870fb063cf9ddae220d36063b6fd6882d5
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-02-07 15:24:49 -0800

    Added board to pong game

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Game from './partials/Game';
 
 // create a game instance
 const game = new Game('game', 512, 256);
-//below is an IIFE. gameLoop function is being called as a recursive.
+//below is an IIFE. gameLoop function is being called as a recursive
 (function gameLoop() {
   game.render();
   requestAnimationFrame(gameLoop);
