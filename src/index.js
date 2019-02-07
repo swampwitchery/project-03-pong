d21664cedd672b5a9f91375c786d477a51b8f664
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-02-07 13:07:12 -0800

    Inital commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles/game.css';
+import Game from './partials/Game';
+
+// create a game instance
+const game = new Game('game', 512, 256);
+//below is an IIFE. gameLoop function is being called as a recursive.
+(function gameLoop() {
+  game.render();
+  requestAnimationFrame(gameLoop);
+})();
